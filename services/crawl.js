221af71157376e84025e2fa53e0926f0aafbe9d3
trung1709 <taductrung1709@gmail.com>
2024-04-12 17:21:05 +0700

    update argument chrome driver

--- services/crawl.js
+++ services/crawl.js
@@ -43,7 +43,7 @@ const {
   getTableCode,
 } = require("./crawl.util.js");
 const { insertReportToDB } = require("../database/insert.js");
-const chrome = require('selenium-webdriver/chrome');
+const chrome = require("selenium-webdriver/chrome");
 
 const waitPageLoad = async (driver) => {
   try {
@@ -61,8 +61,10 @@ const waitPageLoad = async (driver) => {
 
 const crawlData = async () => {
   let options = new chrome.Options();
-  options.addArguments('--headless');
-  options.addArguments('--disable-gpu');
+  options.addArguments("--headless");
+  options.addArguments("--disable-gpu");
+  options.addArguments("--no-sandbox");
+  options.addArguments("--disable-dev-shm-usage");
 
   const driver = await new Builder()
     .forBrowser("chrome")
