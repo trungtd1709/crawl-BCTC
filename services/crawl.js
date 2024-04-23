6d65dfd207cd7322200a7ae7c6315f53a8c9402d
TaDangHuy <taductrung1709@gmail.com>
2024-04-23 17:07:09 +0700

    headless

--- services/crawl.js
+++ services/crawl.js
@@ -66,10 +66,10 @@ const waitPageLoad = async (driver) => {
 
 const crawlData = async () => {
   let options = new chrome.Options().windowSize({width: 1920, height: 1080});
-  // options.addArguments("--headless");
-  // options.addArguments("--disable-gpu");
-  // options.addArguments("--no-sandbox");
-  // options.addArguments("--disable-dev-shm-usage");
+  options.addArguments("--headless");
+  options.addArguments("--disable-gpu");
+  options.addArguments("--no-sandbox");
+  options.addArguments("--disable-dev-shm-usage");
 
   const driver = await new Builder()
     .forBrowser("chrome")
